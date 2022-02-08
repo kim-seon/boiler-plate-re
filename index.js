2c8d3371a7f1126c5bee493486fcd52c1cd8c1b3
kim-seon <91369649+kim-seon@users.noreply.github.com>
2022-02-08 16:00:48 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser')
+const { User } = require('./models/User')
+
+const config = require('./config/key')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://seon:ks12345@boilerplate.ngyb3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
         .then(() => console.log('MongoDB Connected...'))
         .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World! 하이~!')
+})
+
+app.post('/api/users/register', (req, res) => {
+
+    //회원 가입 할떄 필요한 정보들을  client에서 가져오면 
+    //그것들을  데이터 베이스에 넣어준다. 
+    const user = new User(req.body)
+
+    user.save((err, userInfo) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
